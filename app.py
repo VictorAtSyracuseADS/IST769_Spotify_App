960fec55679f34f77ac757eb68d12301b5634884
Victor Yamaykin <97710298+VictorAtSyracuseADS@users.noreply.github.com>
2023-05-08 22:39:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# Streamlit
+# Streamlit App
 
 import streamlit as st
 
@@ -10,10 +10,10 @@ local_css("style.css")
 
 st.title("Spotify App")
 Name_of_Artist = st.text_input("Artist Name")
-Duration = st.number_input("Duration of playlist")
+Duration = st.number_input("Duration of playlist in minutes", min_value = 1, value = 30, step = 1, help = "Enter the length of the playlist using minutes")
 button_clicked = st.button("OK")
 
-# Spotipy
+# Spotify API
 
 from auth import *
 from spotipy_client import *
@@ -35,13 +35,14 @@ for i, item in enumerate(Data['tracks']['items']):
     duration = item['duration_ms']
     need.append((i, track['artists'][0]['name'], track['name'], track_id, song_name, track['release_date'], popularity, duration))
  
-Track_df = pd.DataFrame(need, index=None, columns=('Item', 'Artist', 'Album Name', 'Id', 'Song Name', 'Release Date', 'Popularity', 'Duration (ms)'))
+Track_df = pd.DataFrame(need, index=None, columns=('Item', 'Artist', 'Album Name', 'Id', 'Song Name', 'Release Date', 'Popularity', 'Duration'))
 
-access_token = spotify.access_token
+def convert_ms(duration):
+    minutes = round(duration / 60000, 1)
+    return minutes 
 
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
-endpoint = "https://api.spotify.com/v1/audio-features/"
+Track_df['Duration'] = Track_df['Duration'].apply(convert_ms)
 
-st.table(Track_df[['Artist','Album Name', 'Song Name', 'Popularity', 'Duration (ms)']])
+Result_df = Track_df.loc[:,Track_df['Duration'].sum() < Duration].sort_values(by="Popularity")
+
+st.table(Result_df[['Artist','Album Name', 'Song Name', 'Popularity', 'Duration']])
